d3e41aff339c54bee256007d0e6436e582b16278
Marta Piątek <martusia.piatek@gmail.com>
2018-08-21 22:03:22 +0200

    Pierwszy kontroler

---
+++ src/main/java/pl/martapiatek/SpringBootCourse/Controller.java
@@ -0,0 +1,16 @@
+package pl.martapiatek.SpringBootCourse;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class Controller {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(){
+        return "hello End user";
+    }
+}
